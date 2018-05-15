bcb2049b1e3e8bc220814f144afacf0d454c1c2e
bifidotftw <bifi.ftw@riseup.net>
2018-05-16 00:34:59 +0200

    added TCA specific functions

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
 Scope
 =====
 
-Simulate enrichment for a given metabolic network with constant pool sizes.
-Considered variables:
-- Pool sizes
-- Flux
-Omitted variables:
-- Enzyme kinetics
-- Changing pool sizes
-- All processes are instantaneous, no delays e.g. for transport across membranes
+Simulate enrichment kinetic for a given metabolic network at steady state (= constant metabolite pool sizes).
 
 
 Functionality
 =============
 
-Calculation is done step-wise.
-Flux is driven by the "suction" created by removing (i.e. consuming) metabolites in contrast to pushing metabolites through the network.
-Consequently, each step starts with removing endproducts.
-Subsequently, pools consisting of less molecules than their constant concentration are filled up from upstream metabolite pools.
-Although this behavior is counterintuitive when reading an writing code it is deemed a more accurate representation of reality in conditions where the conditions (nutrient concentrations) are approximately constant.
-A step is finished once all pools are replenished.
-Check $diagram for a detailed behavioral diagram.
+In constant (nutrient) conditions influx and outflux of every metabolite pool and consequently metabolite pool sizes are constant.
+These values are used to simulate the flux in the metabolic network.
 
+Like real metabolic pathways, flux is controlled by a few rate-limiting, and often irreversible, reactions while most other reactions are close to equilibrium and therefore exhibit fast back- and forth reactions, possibly even behaving like a single pool.
+Check $diagram for a detailed behavioral diagram.
 
 Programming Design
 ==================
@@ -29,8 +19,12 @@ Programming Design
 MetSim is employing object oriented programming where each metabolite pool is an object.
 Metabolite pools are stored in pandas dataframes where each column represents a carbon and each row a molecule.
 Unlabeled (12C) carbons are represented by a 0, while labeled (13C) carbons are represented by a 1.
-Consequently a molecule is a sequence of 0s and 1s, for example {'C1': 0, 'C2': 0, 'C3': 1} represents a three-carbons metabolite, e.g. lactate, which is labeled once.
-All necessary functions are defined as methods and ultimately strung together to form a sequence which makes up a step which is iterated to simulate the metabolism.
+Consequently a molecule is a sequence of 0s and 1s, for example {'C1': 0, 'C2': 0, 'C3': 1} represents a three-carbon metabolite, in this case lactate, which is labeled once.
+
+Calculation is done step-wise and every step consists of two sub-steps.
+First, from every metabolite pool random molecules are moved to a intermediate pool called temp and subsequently distributed to the specified neighboring metabolite pools.
+
+All necessary steps are defined as class methods or functions and ultimately strung together to form a sequence which makes up the step which is iterated to simulate the metabolism.
 Enrichment is calculated, stored in a dataframe and can afterwards be saved to csv-files and plotted.
 Check $diagram for a detailed class diagram.
 
@@ -38,10 +32,12 @@ Check $diagram for a detailed class diagram.
 Planned Features
 ================
 
-
+- GUI
+- Use machine learning to generate desired enrichment patterns by changing parameters
 
 
 ToDo
 ====
 
-
+- Write function which calculates enrichment after enrichment has been equilibrated between two pools
+- Calculate enrichment for every carbon
---
+++ metsim.py
@@ -0,0 +1,208 @@
+import pandas as pd
+
+verbose = True
+
+class metabolite_pool(object):
+    
+    def __init__(self, metabolite_name, number_of_carbons, pool_size):
+        self.metabolite_name = metabolite_name
+        self.number_of_carbons = number_of_carbons
+        self.pool_size = pool_size
+
+        self.pool = None
+        self.tmp = None
+
+    def initialize_pool(self):
+        global verbose
+        if verbose:
+            print('running %s.initialize_pool' % self.metabolite_name)
+        #Create dataframe according to number of carbons and pool size with unlabeled molecules
+        columns = {}
+        for i in range(self.number_of_carbons): # create dictionary from C1 to CX (number of carbons with unlabeled carbons (0)
+            columns['C' + str(i+1)] = self.pool_size*[0]
+        self.pool = pd.DataFrame(columns)
+        if verbose:
+            print(self.pool)
+            print('')
+
+    def calculate_enrichment(self, target):
+        global verbose
+        if verbose:
+            print('running %s.calculate_enrichment' % self.metabolite_name)
+        # Total excess
+        number_13C = self.pool.values.sum() # Sum all values
+        #print('Number of 13C-atoms: ' + str(number_13C))
+        number_totalC = self.pool.shape[0]*self.pool.shape[1]
+        #print('Total number of carbons: ' + str(number_totalC))
+        self.total_excess = number_13C/number_totalC
+
+        # Calculate isotopologue distribution
+        # Create dataframe where every row only contains the number of 13C
+        ID = pd.DataFrame({'Number_of_13C': self.pool.sum(axis=1)})
+        # M+1
+        metabolites_M1 = ID[ID.Number_of_13C == 1] # create new dataframe only according to condition
+        count_M1 = metabolites_M1.shape[0] # count rows in dataframe
+        M1 = count_M1/ID.shape[0]
+        # M+2
+        metabolites_M2 = ID[ID.Number_of_13C == 2]
+        count_M2 = metabolites_M2.shape[0]
+        M2 = count_M2/ID.shape[0]
+        # M+3
+        metabolites_M3 = ID[ID.Number_of_13C == 3]
+        count_M3 = metabolites_M3.shape[0]
+        M3 = count_M3/ID.shape[0]
+        # M+4
+        metabolites_M4 = ID[ID.Number_of_13C == 4]
+        count_M4 = metabolites_M4.shape[0]
+        M4 = count_M4/ID.shape[0]
+        # M+5
+        metabolites_M5 = ID[ID.Number_of_13C == 5]
+        count_M5 = metabolites_M5.shape[0]
+        M5 = count_M5/ID.shape[0]
+        # M+6
+        metabolites_M6 = ID[ID.Number_of_13C == 6]
+        count_M6 = metabolites_M6.shape[0]
+        M6 = count_M6/ID.shape[0]
+
+        if target == 'terminal':
+            print('Total Excess: ' + str(self.total_excess))
+            print('M+1: ' + str(M1))
+            print('M+2: ' + str(M2))
+            print('M+3: ' + str(M3))
+            print('M+4: ' + str(M4))
+            print('M+5: ' + str(M5))
+            print('M+6: ' + str(M6))
+            print('')
+
+        if target == 'csv':
+            #TODO: export to csv
+            pass
+
+    def to_tmp(self, number_of_molecules):
+        global verbose
+        if verbose:
+            print('running %s.to_tmp' % self.metabolite_name)
+        self.tmp = self.pool.sample(n = number_of_molecules)
+        self.pool = self.pool.loc[~self.pool.index.isin(self.tmp.index)]
+        if verbose:
+            print(self.tmp)
+            print('')
+    
+    def from_tmp(self, number_of_molecules, source):
+        global verbose
+        if verbose:
+            print('running %s.from_tmp' % self.metabolite_name)
+        source = source.sample(n = number_of_molecules)
+        self.pool = pd.concat([self.pool, source])
+        # Sanity check
+        if self.pool.shape[0] != self.pool_size:
+            print('ERROR in function "%s from_tmp": Pool size changed' % self.metabolite_name)
+            quit()
+        self.pool = self.pool.reset_index(drop=True)
+        if verbose:
+            print(self.pool)
+            print('')
+
+    def introduce_molecule(self, number_of_molecules, molecule):
+        global verbose
+        if verbose:
+            print('running %s.introduce_molecule' % self.metabolite_name)
+        if type(molecule) is not dict:
+            print('ERROR: variable "molecule" in function "introduce_molecule" is not a dictionary.')
+            quit()
+        new_molecules = pd.DataFrame(number_of_molecules*[molecule])
+        self.pool = pd.concat([self.pool, new_molecules])
+        self.pool = self.pool.reset_index(drop=True)
+        # Sanity check
+        if self.pool.shape[0] != self.pool_size:
+            print('ERROR in function "introduce_molecule": Pool size of %s changed' % self.metabolite_name)
+            quit()
+        if verbose:
+            print(self.pool)
+        
+
+    def mirror_symmetry(self):
+        #TODO: generalize mirror symmetry
+        pass
+        
+# Metabolite conversions
+
+def oxaloacetate_to_citrate(number_of_molecules):
+    if verbose:
+        print('running oxaloacetate_to_citrate')
+    tmp1 = oxaloacetate.tmp.sample(n = number_of_molecules)
+    tmp1 = tmp1.rename(columns={'C1': 'C6', 'C2': 'C3', 'C3': 'C4', 'C4': 'C5'}) # Rename carbons according to new position in citrate, side chain is assigned C6
+    tmp1 = tmp1.reset_index(drop = True)
+
+    tmp2 = pyruvate.tmp.sample(n = number_of_molecules)
+    # TODO: check which carbon of pyruvate is removed
+    tmp2 = tmp2.drop(['C1'], axis = 1)
+    tmp2 = tmp2.rename(columns = {'C2': 'C1', 'C3': 'C2'})
+    tmp2 = tmp2.reset_index(drop = True)
+
+    oxaloacetate_to_citrate.tmp = tmp1.join(tmp2)
+
+    if verbose:
+        print(oxaloacetate_to_citrate.tmp)
+        print('')
+
+def citrate_to_glutamate(number_of_molecules):
+    if verbose:
+        print('running citrate_to_glutamate')
+    tmp = citrate.pool.sample(n = number_of_molecules)
+    citrate_to_glutamate.tmp = tmp.drop(['C6'], axis = 1)
+    if verbose:
+        print(citrate_to_glutamate.tmp)
+        print('')
+
+def glutamate_to_succinate(number_of_molecules):
+    if verbose:
+        print('running glutamate_to_succinate')
+    tmp = glutamate.pool.sample(n = number_of_molecules)
+    glutamate_to_succinate.tmp = tmp.drop(['C5'], axis = 1)
+    if verbose:
+        print(glutamate_to_succinate.tmp)
+        print('')
+    
+
+
+
+# Testing
+
+#citrate = metabolite_pool('citrate', 6, 10)
+#citrate.initialize_pool()
+#citrate.to_tmp(10)
+#citrate.introduce_molecule(10, {'C1': 1, 'C2': 1, 'C3': 1, 'C4': 1, 'C5': 1, 'C6': 1})
+#citrate_to_glutamate(1)
+#
+#glutamate = metabolite_pool('glutamate', 5, 20)
+#glutamate.initialize_pool()
+#glutamate.to_tmp(1)
+#glutamate.from_tmp(1, citrate_to_glutamate.tmp)
+#
+#succinate = metabolite_pool('succinate', 4, 10)
+#succinate.initialize_pool()
+#succinate.to_tmp(5)
+#glutamate_to_succinate(5)
+#succinate.from_tmp(5, glutamate_to_succinate.tmp)
+
+
+# TCA cycle
+
+# Initialte pools
+pyruvate = metabolite_pool('pyruvate', 3, 10) # will be replenished infinitely with labeled carbons
+pyruvate.initialize_pool()
+
+citrate = metabolite_pool('citrate', 6, 10)
+citrate.initialize_pool()
+
+glutamate = metabolite_pool('glutamate', 5, 10)
+glutamate.initialize_pool()
+
+aspartate = metabolite_pool('aspartate', 4, 10)
+aspartate.initialize_pool()
+
+
+# move molecules to tmp for every metabolite pool
+#TODO: rerwite from_tmp and metabolite specific functions in a way that they 'consume' molecules from tmp, i.e. they delete the ones they used
+# Implementation: see to_tmp, make class.tmp variables writable from outside?
